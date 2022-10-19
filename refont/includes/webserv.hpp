a4e526d45c7e5df97fe2a26c37e681835d7fb026
6L45 <silas.dbr170@gmail.com>
2022-10-19 17:24:20 +0200

    implementation de la class server gestion de plusieurs sockets

--- refont/includes/webserv.hpp
+++ refont/includes/webserv.hpp
@@ -1,15 +1,31 @@
 #ifndef WEBSERV_HPP
 # define WEBSERV_HPP
+
 #include "common.hpp"
 
 class Server
 {
 	public :
-		Server(int port) {};
+		Server(int port);
+		Server() { this->_sock = -1; }; //empty serv
 		~Server() {};
 
-	private :
+		void		request_handler(int &fd);
+		int			&get_socket(void);
+		void		print_request_client(int fd);
+		void		send_response(int fd, fd_set &current_sockets);
 
+	private :
+		struct	sockaddr_in		_address;
+		int						_sock;
+		int						_domain;
+		int						_service;
+		int						_protocol;
+		int						_port;
+		u_long					_interface;
+
+		char					_buff[MAXLINE + 1];
+		char					*_recvline;
 };
 
 class Webserv
@@ -23,30 +39,13 @@ class Webserv
 		void 	launch();
 
 	private :
-	//------------------------------------------------------
-		struct	sockaddr_in		_address;	//|
-		struct	sockaddr_in		_address2;	//|__ pas besoin de 2 de chaque dans serv
-		int						_sock;		//|    c'etait pour tests sur plusieurs sockets dans Webserv
-		int						_sock2;		//|
-		int						_domain;
-		int						_service;
-		int						_protocol;
-		int						_port;
-		u_long					_interface;
-
-		char					_buff[MAXLINE + 1];
-		char					*_recvline;
-	//------------------------------------------------------- à passer dans Server
-
 		int						_confd;
-		std::list<int>			_server_fd; // list des fd à surveiller pour le accept
-		std::map<int, int>		_fds;		// <- fd connexion (this->_connfd) // second fd server correspondant
-		std::vector<Server>		_servs;		// prévoir 0 1 2 vide pour appeler les server directement par leur fd
-											// exemple : this->_servs[map[ntohs(client.sin_port)].second].method()
+		std::vector<int>		_server_fd;
+		std::map<int, int>		_fds;
+		std::vector<Server>		_servs;
 
-		// private methods
-		void	__print_request_client(void); // a passer dans Server (handle_request) avec valeur de retour (HTTP protocole)
-		void	__bind_and_listen(void); // Server construc (+ ou -)
+		bool	__is_a_socket(int fd);
+
 };
 
 #endif
---
+++ refont/srcs/server.cpp
@@ -0,0 +1,86 @@
+#include "webserv.hpp"
+
+Server::Server(int port)
+{
+	this->_domain =		AF_INET;
+	this->_service =	SOCK_STREAM;
+	this->_protocol =	0;
+	this->_port =		port;
+	this->_interface =	INADDR_ANY;
+
+	this->_address.sin_family =			this->_domain; //AF_INET
+	this->_address.sin_port =			htons(this->_port);
+	this->_address.sin_addr.s_addr =	htonl(this->_interface); // INADRR_ANY
+
+	// socket creation 
+	this->_sock = socket(this->_domain, this->_service, this->_protocol);
+	std::cout << "==> " << this->_sock << std::endl;
+
+	// bind socket to a port
+	if ( (bind(this->_sock, (struct sockaddr *)&(this->_address), 
+					sizeof(this->_address))) < 0 )
+	{
+		perror("Error : bind socket");
+		exit(EXIT_FAILURE);
+	}
+
+	// listen to the port
+	if ( (listen(this->_sock, 10)) < 0 ) // <--- second arg = backlog : defines the maximum length to which the queue of pending connections for sockfd may grow
+	{
+		perror("Error : listen");
+		exit(EXIT_FAILURE);
+	}
+
+	std::cout << "connected and listening" << std::endl;
+}
+
+int	&Server::get_socket()
+{
+	return (this->_sock);
+}
+
+void	Server::print_request_client(int fd)
+{
+	int	n;
+
+	this->_recvline = static_cast<char *>(malloc(MAXLINE));
+	memset(this->_recvline, 0, MAXLINE);
+	std::cout << std::endl;
+	while ( (n = recv(fd, this->_recvline, MAXLINE - 1, MSG_DONTWAIT)) > 0 )
+	{
+		std::cout << this->_recvline << std::endl << std::endl;
+	}
+	if (n < 0)
+	{
+		if (errno != EAGAIN)
+		{
+			perror("read error");
+			close(fd);
+		}
+	}
+	free(this->_recvline);
+}
+
+void	Server::send_response(int fd, fd_set &current_sockets)
+{
+	std::string server_message = "HTTP/1.1 200 OK\r\n\
+Content-Type: text/html\r\n\
+Content-Length: 55\r\n\
+Keep-Alive: timeout=5, max=1000\r\n\
+Connection: Keep-Alive\r\n\
+\r\n";
+	server_message += std::to_string(this->_sock);
+
+	std::signal(SIGPIPE, SIG_IGN); // ignorer le sigpipe car sinon crash
+	if ((send(fd, server_message.c_str(), server_message.length(), 0)) < 0)
+	{
+		if (errno == EPIPE)
+		{
+			std::cout << "++connexion with client is lost, closing fd : " << fd << "++" << std::endl;
+			FD_CLR(fd, &current_sockets);
+			close(fd);
+		}
+	}
+	else
+		std::cout << "Message send to the client fd : " << fd << "++" << std::endl;
+}
